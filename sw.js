14688457ffda4bcb5d5ceea6a2c5ca435ff29df8
Siddhant <88941314+SidIsGitHub@users.noreply.github.com>
2025-12-25 13:14:54 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,3 +8,11 @@ self.addEventListener("install", (e) => {
 self.addEventListener("fetch", (e) => {
     e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
+self.addEventListener("install", (e) => {
+    self.skipWaiting(); // 🚀 Forces the new service worker to become active immediately
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(assets)));
+});
+
+self.addEventListener("activate", (e) => {
+    e.waitUntil(clients.claim()); // 🚀 Forces the new worker to take control of the page instantly
+});
